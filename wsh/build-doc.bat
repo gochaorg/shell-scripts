c33d94fb71aff0495925c82429b08a2cee0dbc61
Kamnev Georgiy <nt.gocha@gmail.com>
2015-11-13 10:30:12 +0500

    removed bad docs

--- wsh/build-doc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-:: directory of this file
-set SCRIPT_DIR=%~dp0
-set rm=%SCRIPT_DIR%bin\rm.exe
-
-%rm% -rfv %SCRIPT_DIR%doc\*
-call jsdoc.bat --directory=%SCRIPT_DIR%doc\ %SCRIPT_DIR%\jslib
